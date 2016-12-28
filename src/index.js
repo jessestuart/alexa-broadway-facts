2bfc3e85b90805b189b2dc3af0af52d30e12c795
Jesse Stuart <jdstuart@icloud.com>
2016-12-28 02:27:00 -0500

    [data] Add broadway facts!

--- src/index.js
+++ src/index.js
@@ -1,78 +1,38 @@
 'use strict';
 var Alexa = require('alexa-sdk');
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+var APP_ID = 'amzn1.ask.skill.8a9ee741-26eb-4928-8c08-9147dab20d4e';
 
 var languageStrings = {
-    "en-GB": {
-        "translation": {
-            "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
-            ],
-            "SKILL_NAME" : "British Space Facts",
-            "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
-            "HELP_REPROMPT" : "What can I help you with?",
-            "STOP_MESSAGE" : "Goodbye!"
-        }
-    },
     "en-US": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                "Broadway Theaters are mostly not on Broadway There are 40 Broadway theaters, but only 4 are actually on Broadway.",
+                "Broadway theaters were mostly downtown in the 19th century. Not until the turn of the century did they begin to cluster around what became Times Square.",
+                "A Broadway theater doesn’t have to be on Broadway, but it does have to seat at least 500 people.",
+                "There is no row ‘I’ in most Broadway theaters. This is to avoid disappointing people who might have thought they were sitting in row 1.",
+                "The longest running Broadway show in history is 'Phantom of the Opera', with over 12,000 performances and counting.",
+                "The highest grossing Broadway show to date is 'The Lion King', grossing $1.32 billion to date.",
+                "The first Tony Awards were held in 1947.",
+                "The most highly-awarded musical ever was 'The Producers', which won 12 Tony awards.",
+                "In 2013-2014, 70% of Broadway tickets were bought by tourists.",
+                "Broadway audiences tend to skew female. In 2013-2014, 68% of Broadway theatergoers were female.",
+                "No actor named Tony has ever won a Tony.",
+                "The first Broadway theater, 'The Playhouse', was opened in the 1730s on Broadway in downtown Manhattan, between Beaver and Exchange Place.",
+                "Attendance at Broadway shows has risen 13.3% between 2014 and 2016.",
+                "Broadway was once commonly referred to as the 'Great White Way'.",
+                "More than 13 million tickets were sold to Broadway productions in 2016; more than any year in history.",
+                "The Booth Theatre was named in memory of one of America’s greatest 19th century classical and Shakespearean actors, Edwin Booth (brother of Abraham Lincoln assassin, John Wilkes Booth).",
+                "Mae West’s 1926 play 'Sex', the story of a Montreal prostitute, ran for a year before New York's deputy police commissioner charged the theater company with lewdness and the corruption of youth. She spent 10 days in jail.",
+                "The first nude Broadway musical was Hair, which opened in 1968 and ran 1750 performances.",
+                "Audra McDonald has won six Tony Awards for performance, more than any other individual. She is the only person to have won Tony Awards in four different acting categories.",
+                "Only one cast replacement has ever been nominated for a Tony Award in a competitive category. Larry Kert was nominated for Best Actor in a Musical for his performance in Company (1971). Dean Jones originated the role (and can be heard on the cast album) but left the production soon after the opening.",
+                "Stephen Sondheim has received eight Tony Awards, more than any other composer."
             ],
-            "SKILL_NAME" : "American Space Facts",
+            "SKILL_NAME" : "Broadway Theatre Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me a Broadway fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
-        }
-    },
-    "de-DE": {
-        "translation": {
-            "FACTS": [
-                "Ein Jahr dauert auf dem Merkur nur 88 Tage.",
-                "Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.",
-                "Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.",
-                "Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.",
-                "Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.",
-                "Jupiter hat den kürzesten Tag aller Planeten.",
-                "Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.",
-                "Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.",
-                "Die Sonne ist eine fast perfekte Kugel.",
-                "Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.",
-                "Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.",
-                "Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.",
-                "Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr."
-            ],
-            "SKILL_NAME" : "Weltraumwissen auf Deutsch",
-            "GET_FACT_MESSAGE" : "Hier sind deine Fakten: ",
-            "HELP_MESSAGE" : "Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?",
-            "HELP_REPROMPT" : "Wie kann ich dir helfen?",
-            "STOP_MESSAGE" : "Auf Wiedersehen!"
         }
     }
 };
